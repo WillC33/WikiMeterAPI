8541e49bcbc5fe29e0386dccf317e08aac9acba9
WillC33 <williamcookenet@pm.me>
2023-11-13 17:25:46 +0000

    feat(Program): implemented token rate limited

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.RateLimiting;
 using WikiMeterApi;
 
@@ -10,15 +11,30 @@ builder.Services.AddCors();
 builder.Services.AddMemoryCache();
 builder.Services.AddDistributedMemoryCache();
 
-//TODO: configure rate limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.AddTokenBucketLimiter("token", bucketOptions =>
+    {
+        bucketOptions.TokenLimit = 100;
+        bucketOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        bucketOptions.QueueLimit = 5;
+        bucketOptions.ReplenishmentPeriod = TimeSpan.FromSeconds(10);
+        bucketOptions.TokensPerPeriod = 20;
+        bucketOptions.AutoReplenishment = true;
+    });
+});
 
 
 var app = builder.Build();
 
 app.UseCors(options => options.AllowAnyOrigin());
 
+app.UseRateLimiter();
+
 app.MapGet("/api/wikipedia", async (IWikipediaDataService wikipediaDataService) =>
-await wikipediaDataService.GetWikipediaDataAsync()
-);
+        await wikipediaDataService.GetWikipediaDataAsync()
+    )
+    .RequireRateLimiting("token");
 
 app.Run();
